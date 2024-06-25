88d9fa7cb67d259989e2da748c70c027d31882e2
Milton Jonathan <milton.jonathan@cartesi.io>
2024-06-25 15:57:06 -0300

    Remove root arg from CelestiaRelay.relayShares
    Root can already be extracted from SharesProof obj

--- onchain/contracts/CelestiaRelay.sol
+++ onchain/contracts/CelestiaRelay.sol
@@ -21,30 +21,31 @@ contract CelestiaRelay {
         blobstreamX = _blobstreamX;
     }
 
-    /// @notice Relay the specified Celestia shares as an input to a DApp's input box
-    /// @dev Called by clients to securely relay Celestia data to Cartesi DApps
+    /// @notice Relay a pointer to the specified Celestia shares as an input to a DApp's input box
+    /// @dev Called by clients to securely process Celestia data in Cartesi DApps
     /// @param _dapp The address of the DApp
     /// @param _proof SharesProof object showing that a range of shares is committed to Celestia
-    /// @param _root The Celestia "dataRoot" with the commitment for the block that contains the shares
     /// @param _execLayerData Additional data to be interpreted by the execution layer
     /// @return The hash of the input as returned by the Cartesi DApp's input box
     function relayShares(
         address _dapp,
         SharesProof memory _proof,
-        bytes32 _root,
         bytes calldata _execLayerData
     ) external returns (bytes32)
     {
+        // retrieve "dataRoot" from proof (commitment for Celestia block that contains the shares)
+        bytes32 dataRoot = _proof.attestationProof.tuple.dataRoot;
+
         // verify if shares are committed to the Celestia Blobstream contract
         (bool verified, DAVerifier.ErrorCodes errorCode) =
             DAVerifier.verifySharesToDataRootTupleRoot(
                 blobstreamX,
                 _proof,
-                _root
+                dataRoot
             );
         require(verified, "Shares not committed to Celestia BlobstreamX contract");
 
-        // relay data as Cartesi DApp input
+        // relay data specification as a Cartesi DApp input
         // input payload corresponds to the ABI encoding of the following fields:
         // - namespace: Celestia identifier for the DApp (29 bytes)
         // - dataRoot: commitment for the Celestia block that contains the relayed data (32 bytes)
@@ -53,7 +54,6 @@ contract CelestiaRelay {
         // - end: index of the final "share" (chunk) for the relayed data within the given Celestia block (32 bytes)
         // - execLayerData: arbitrary additional data to be interpreted by the DApp code
         Namespace memory namespace = _proof.namespace;
-        bytes32 dataRoot = _proof.attestationProof.tuple.dataRoot;
         uint256 blockHeight = _proof.attestationProof.tuple.height;
         uint256 start = _proof.shareProofs[0].beginKey;
         uint256 end = _proof.shareProofs[_proof.shareProofs.length-1].endKey;
